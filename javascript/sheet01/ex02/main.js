0817b10a00cbf95021db6758136306aaf4e0919d
Miguel Ligero <milica0479@gmail.com>
2024-06-17 10:43:40 +0200

    Show variables and its type in console

---
+++ javascript/sheet01/ex02/main.js
@@ -0,0 +1,7 @@
+let number1 = 45;
+let string1 = "Que tal";
+let boolean1 = true;
+
+console.log(number1 +  " is of type " + typeof number1);
+console.log(string1 +  " is of type " + typeof string1);
+console.log(boolean1 +  " is of type " + typeof boolean1);
