eee601596fb798a2506b88d5547b151fe4af4f9a
Miguel Ligero <miguel.ligero79@gmail.com>
2024-06-27 19:14:30 +0200

    Falta terminar el css

--- frontend/sheet01/pokemon/main.js
+++ frontend/sheet01/pokemon/main.js
@@ -1,23 +1,41 @@
 async function getListPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/';
+    try {
+        const url = "https://pokeapi.co/api/v2/pokemon/";
+
+        const pokemonListResponse = await fetch(url);
+        let pokemonListJson;
+
+        if (pokemonListResponse.status >= 200 && pokemonListResponse.status <= 299) {
+            pokemonListJson = await pokemonListResponse.json();
+        } else {
+            throw new Error('Error: Estado ' + pokemonListResponse.status);
+        }
+
+        const pokemonData = await getPokemonData(pokemonListJson.results);
+
+        createCard(pokemonData);
+
+    } catch (err) {
+        console.error(err);
+    }
 
     // "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20"
 
-    await fetch(url)
-        .then(response => {
-            if (response.status >= 200 && response.status <= 299) {
-                return response.json();
-            }
-        })
-        .then(json => {
-            return getPokemonData(json.results);
-        })
-        .then(pokemonData => {
-            createCard(pokemonData);
-        })
-        .catch(err => {
-            throw new Error('error: ' + err);
-        });
+    /*await fetch(url)
+          .then(response => {
+              if (response.status >= 200 && response.status <= 299) {
+                  return response.json();
+              }
+          })
+          .then(json => {
+              return getPokemonData(json.results);
+          })
+          .then(pokemonData => {
+              createCard(pokemonData);
+          })
+          .catch(err => {
+              throw new Error('error: ' + err);
+          });*/
 }
 
 async function getPokemonData(urls) {
@@ -27,11 +45,11 @@ async function getPokemonData(urls) {
         for (const key in urls) {
             if (Object.hasOwnProperty.call(urls, key)) {
                 let pokemonTypes = [];
-                const element = urls[key]
-                const pokemonResponse = await fetch(element.url)
-                const pokemonJson = await pokemonResponse.json()
+                const element = urls[key];
+                const pokemonResponse = await fetch(element.url);
+                const pokemonJson = await pokemonResponse.json();
 
-                pokemonJson.types.forEach(element => {
+                pokemonJson.types.forEach((element) => {
                     pokemonTypes.push(element.type.name);
                 });
 
@@ -40,7 +58,7 @@ async function getPokemonData(urls) {
                     name: pokemonJson.name,
                     types: pokemonTypes,
                     parent: await getPokemonParent(pokemonJson.species.url),
-                    img: pokemonJson.sprites.front_default
+                    img: pokemonJson.sprites.front_default,
                 });
             }
         }
@@ -49,10 +67,9 @@ async function getPokemonData(urls) {
 
     } catch (err) {
         console.error(err);
+        throw new Error('Error: Mensaje: ' + err);
     }
 }
-
-const urlParent = "https://pokeapi.co/api/v2/pokemon-species/";
 
 async function getPokemonParent(speciesUrl) {
     try {
@@ -62,66 +79,63 @@ async function getPokemonParent(speciesUrl) {
         if (parentJson.evolves_from_species != null) {
             return parentJson.evolves_from_species.name;
         }
+
         return null;
+
     } catch (err) {
         console.error(err);
+        throw new Error('Error: Mensaje: ' + err);
     }
 }
 
-
 function createCard(pokemonData) {
-    let container = document.getElementById('cardContainer');
+    const container = document.getElementById("cardContainer");
 
     for (const key in pokemonData) {
         if (Object.hasOwnProperty.call(pokemonData, key)) {
             const element = pokemonData[key];
+            const card = document.createElement("div");
+            card.className  = "card";
 
-            let card = document.createElement('div');
-
-            card.innerHTML = `<div class="card">
-                        <div class="imgContainer">
-                            <img src="${element.img}" alt="Avatar" style="width:100%">
-                            <p class="pokemonIdLabel">ID/${element.id}</p>
-                        </div>
-                        <div class="dataContainer">
-                            <h4><b>${element.name}</b></h4>
-                            <div id="typeContainer${element.id}" class="typeContainer">
-                                <span class="pokemonType">Grass</span>
-                                <span class="pokemonType">Fire</span>
-                            </div>
-                            
-                            <div class="evolutionContainer">
-                                <p>Evoluciona de:</p>
-                                <h4>${element.parent}</h4>
-                            </div>
-                        </div>
-                    </div>`;
+            card.innerHTML =    `<div class="imgContainer">
+                                    <img src="${element.img}" alt="Imaagen pokemon" style="width:100%">
+                                    <p class="pokemonIdLabel">ID/${element.id}</p>
+                                </div>
+                                <div class="dataContainer">
+                                    <h4><b>${capitalizeFirstLetter(element.name)}</b></h4>
+                                    <div id="typeContainer${element.id}" class="typeContainer"></div>
+                                    <div id="evolutionContainer${element.id}" class="evolutionContainer"></div>
+                                </div>`;
 
             container.appendChild(card);
 
-            let divTypeContainer = document.getElementById(`typeContainer${element.id}`);
-            divTypeContainer.className = 'typeContainer';
-            console.log(element.types);
-            let span = '';
+            let span = "";
+            const divTypeContainer = document.getElementById(`typeContainer${element.id}`);
+
+            divTypeContainer.className = "typeContainer";
+
             for (const key in element.types) {
                 if (Object.hasOwnProperty.call(element.types, key)) {
                     const type = element.types[key];
 
-                    
-                    span += `<span class="pokemonType">${type}</span>`;
-                    divTypeContainer.appendChild;
-                    
+                    span += `<span class="pokemonType">${capitalizeFirstLetter(type)}</span>`;
                 }
             }
             divTypeContainer.innerHTML = span;
-            console.log(divTypeContainer);
+
+            const divEvolutionContainer = document.getElementById(`evolutionContainer${element.id}`);
 
             if (element.parent != null) {
-
+                let h4 = document.createElement("h4");
+                h4.innerHTML = `<p>Evoluciona de:</p><h4>${capitalizeFirstLetter(element.parent)}</h4>`;
+                divEvolutionContainer.appendChild(h4);
             }
         }
     }
 }
 
-getListPokemon();
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
+getListPokemon();
