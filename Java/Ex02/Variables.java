0e1f2f9418c5486fdc3048d1cab341bd6e6948d4
Miguel Ligero <milica0479@gmail.com>
2024-07-09 13:12:23 +0200

    Variables and data types

--- Java/Ex02/Variables.java
+++
@@ -1,5 +0,0 @@
-package Java.Ex02;
-
-public class Variables {
-    
-}
---
+++ Java/Ex02/VariablesTipos.java
@@ -0,0 +1,19 @@
+package Java.Ex02;
+
+public class VariablesTipos {
+    public static void main(String[] args) {
+        int entero = 4;
+        double doble = 2.5d;
+        float flotante = 3.6f;
+        String cadena = "Un texto";
+        boolean boleano = true;
+        char caracter = 'c';
+
+        System.out.println("Entero: " + entero);
+        System.out.println("Doble: " + doble);
+        System.out.println("Flotante: " + flotante);
+        System.out.println("Cadena: " + cadena);
+        System.out.println("Boleano: " + boleano);
+        System.out.println("Caracter: " + caracter);
+    }
+}
