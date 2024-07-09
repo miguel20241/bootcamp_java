dcb92ba00a57a2159b11b8492cc01288a41a27ef
Miguel Ligero <miguel.ligero79@gmail.com>
2024-07-09 16:34:26 +0200

    Use arrays

--- Java/Ex06/Arrays.java
+++ Java/Ex06/Arrays.java
@@ -1,5 +1,15 @@
 package Java.Ex06;
 
 public class Arrays {
-    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        arr[0] = 10;
+        arr[1] = 9;
+        arr[2] = 8;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
