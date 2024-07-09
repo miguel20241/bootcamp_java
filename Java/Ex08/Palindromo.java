37031cad6c8e4ae1eef2ed38a37c461e4b2bbb95
Miguel Ligero <miguel.ligero79@gmail.com>
2024-07-09 17:39:47 +0200

    String handling

--- Java/Ex08/Palindromo.java
+++ Java/Ex08/Palindromo.java
@@ -1,5 +1,51 @@
 package Java.Ex08;
 
+import java.util.Scanner;
+
 public class Palindromo {
-    
+    public static void main(String[] args) {
+        
+        boolean isPalindrome = true;
+
+        Scanner scanObj = new Scanner(System.in);
+
+        System.out.println("Introduzca un texto:");
+
+        if (scanObj.hasNextLine()) {
+            String text = scanObj.nextLine();
+            // Curiosa utilización de un bucle for
+            for(int i = 0, j = text.length() - 1; i <= j; i++, j--){
+                if (text.charAt(i) != text.charAt(j)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            System.out.println("La palabra " + text + (isPalindrome ? " es": " no es") + " un palíndromo");
+        } else {
+            System.out.println("No es una cadena válida");
+        }
+        
+        scanObj.close();
+    }
+
+    /* public static void main(String[] args) {
+        Scanner scanObj = new Scanner(System.in);
+
+        System.out.println("Introduzca un texto:");
+
+        if (scanObj.hasNextLine()) {
+            String text = scanObj.nextLine();
+            String reverseText = "";
+            for (int i = text.length() - 1; i >= 0; i--) {
+                reverseText += text.charAt(i);
+            }
+            
+            System.out.println("La palabra " + text + (reverseText.equals(text) ? " es": " no es") + " un palíndromo");
+        } else {
+            System.out.println("No es una cadena válida");
+        }
+
+        scanObj.close();
+    } */
 }
