752b96fef158e38a92dbc0871c4c2b154fb8b88e
Miguel Ligero <miguel.ligero79@gmail.com>
2024-07-09 16:29:11 +0200

    Use For Loop

--- Java/Ex05/BucleFor.java
+++ Java/Ex05/BucleFor.java
@@ -1,1 +1,9 @@
+package Java.Ex05;
 
+public class BucleFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
