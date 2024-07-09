f51adb4b4aee3263b7e67735ee71a09eb18d3306
Miguel Ligero <milica0479@gmail.com>
2024-07-09 13:41:39 +0200

    Close scanner

--- Java/Ex03/OperacionesAritmeticas.java
+++ Java/Ex03/OperacionesAritmeticas.java
@@ -14,10 +14,13 @@ public class OperacionesAritmeticas {
         System.out.println("Suma de " + num1 + " + " + num2 + " = " + (num1 + num2));
         System.out.println("Resta de " + num1 + " - " + num2 + " = " + (num1 - num2));
         System.out.println("Multiplicación de " + num1 + " * " + num2 + " = " + (num1 * num2));
+        
         if (num2 == 0) {
             System.out.println("No se puede dividir entre 0");
         } else {
             System.out.println("División de " + num1 + " / " + num2 + " = " + (num1 / num2));
         }
+
+        scanObj.close();
     }
 }
