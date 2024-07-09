df8ff24201451886c21434d7cd55ae3c1ccab2d2
Miguel Ligero <miguel.ligero79@gmail.com>
2024-07-09 17:50:17 +0200

    Data Input and Output

--- Java/Ex09/Saludo.java
+++ Java/Ex09/Saludo.java
@@ -1,5 +1,16 @@
 package Java.Ex09;
 
+import java.util.Scanner;
+
 public class Saludo {
-    
+    public static void main(String[] args) {
+        Scanner scanObj = new Scanner(System.in);
+
+        System.out.println("Introduzca su nombre:");
+
+        String name = scanObj.nextLine();
+        System.out.println("Hola, Don " + name + "!!");
+        
+        scanObj.close();
+    }
 }
