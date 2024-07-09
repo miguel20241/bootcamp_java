6533e74a151260c6bb7fe0203d20965052d305e8
Miguel Ligero <miguel.ligero79@gmail.com>
2024-07-09 16:52:32 +0200

    Use function

--- Java/Ex07/Funciones.java
+++ Java/Ex07/Funciones.java
@@ -1,5 +1,15 @@
 package Java.Ex07;
 
 public class Funciones {
-    
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 89;
+
+        int sum = suma(num1, num2);
+        System.out.println("La suma es: " + sum);
+    }
+
+    public static int suma(int num1, int num2) {
+        return num1 + num2;
+    }
 }
