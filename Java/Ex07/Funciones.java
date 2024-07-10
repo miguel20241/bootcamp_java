d86744aec0c05092529fc4b761e9158a9f04786b
Miguel Ligero <milica0479@gmail.com>
2024-07-10 10:12:42 +0200

    Passing arguments to main method

--- Java/Ex07/Funciones.java
+++ Java/Ex07/Funciones.java
@@ -2,11 +2,10 @@ package Java.Ex07;
 
 public class Funciones {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 89;
-
-        int sum = suma(num1, num2);
-        System.out.println("La suma es: " + sum);
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        
+        System.out.println("La suma es: " + suma(num1, num2));
     }
 
     public static int suma(int num1, int num2) {
