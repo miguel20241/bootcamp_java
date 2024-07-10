b64ff62a285a5db09db55c16d801c9de29d99490
Miguel Ligero <milica0479@gmail.com>
2024-07-10 12:10:42 +0200

    Check input arguments

--- Java/Ex08/Palindromo.java
+++ Java/Ex08/Palindromo.java
@@ -5,27 +5,33 @@ import java.util.Scanner;
 public class Palindromo {
     public static void main(String[] args) {
         
-        boolean isPalindrome = true;
+        //boolean isPalindrome = true;
         Scanner scanObj = new Scanner(System.in);
 
         System.out.println("Introduzca un texto:");
         String text = scanObj.nextLine();
 
         if (!text.isEmpty()) {
-            // Curiosa utilización de un bucle for
-            for(int i = 0, j = text.length() - 1; i <= j; i++, j--){
-                if (text.charAt(i) != text.charAt(j)) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
-
-            System.out.println("La palabra " + text + (isPalindrome ? " es": " no es") + " un palíndromo");
+            System.out.println("La palabra " + text + (isPalindrome(text) ? " es": " no es") + " un palíndromo");
         } else {
             System.out.println("No es una cadena válida");
         }
-        
+
         scanObj.close();
+    }
+    
+    static boolean isPalindrome(String text) {
+        /* for(int i = 0, j = text.length() - 1; i <= j; i++, j--){
+            if (text.charAt(i) != text.charAt(j)) {
+                return false;
+            }
+        } */
+        for(int i = 0; i <= text.length() / 2; i++){
+            if (text.toLowerCase().charAt(i) != text.toLowerCase().charAt(text.length() - (i + 1))) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /* public static void main(String[] args) {
