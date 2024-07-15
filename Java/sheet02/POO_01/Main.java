17fc2eea88728506f64461e91cc8af272da08242
Miguel Ligero <milica0479@gmail.com>
2024-07-15 12:02:29 +0200

    Create getter and setter

--- Java/sheet02/POO_01/Main.java
+++ Java/sheet02/POO_01/Main.java
@@ -2,14 +2,17 @@ package Java.sheet02.POO_01;
 
 public class Main {
     public static void main(String[] args) {
-        Persona persona = new Persona("Miguel", 45);
-
         Main main = new Main();
+        Persona persona = new Persona("Miguel", 45);
+        main.mostrarDetalles(persona);
+        
+        persona.setName("Carlos");
+        persona.setEdad(30);      
         main.mostrarDetalles(persona);
     }
 
     public void mostrarDetalles(Persona persona) {
-        System.out.println(persona.name);
-        System.out.println(persona.edad);
+        System.out.println(persona.getName());
+        System.out.println(persona.getEdad());
     }
 }
--- Java/sheet02/POO_01/Persona.java
+++ Java/sheet02/POO_01/Persona.java
@@ -1,12 +1,28 @@
 package Java.sheet02.POO_01;
 
 public class Persona {
-    String name;
-    int edad;
+    private String name;
+    private int edad;
 
     public Persona(String name, int edad) {
         this.name = name;
         this.edad = edad;
         
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 }
