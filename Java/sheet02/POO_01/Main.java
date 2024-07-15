8fd703e8c2ea6542e42a92124f1742578ddde0ea
Miguel Ligero <milica0479@gmail.com>
2024-07-15 11:56:41 +0200

    Create method mostrarDetalles()

--- Java/sheet02/POO_01/Main.java
+++ Java/sheet02/POO_01/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Persona persona = new Persona("Miguel", 45);
 
+        Main main = new Main();
+        main.mostrarDetalles(persona);
+    }
+
+    public void mostrarDetalles(Persona persona) {
         System.out.println(persona.name);
         System.out.println(persona.edad);
     }
