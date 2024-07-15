ba657a5fc0760e1bbcc70f67ce69ff149825bcf5
Miguel Ligero <milica0479@gmail.com>
2024-07-15 11:50:17 +0200

    Create class Persona

---
+++ Java/sheet02/POO_01/Main.java
@@ -0,0 +1,10 @@
+package Java.sheet02.POO_01;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona("Miguel", 45);
+
+        System.out.println(persona.name);
+        System.out.println(persona.edad);
+    }
+}
---
+++ Java/sheet02/POO_01/Persona.java
@@ -0,0 +1,12 @@
+package Java.sheet02.POO_01;
+
+public class Persona {
+    String name;
+    int edad;
+
+    public Persona(String name, int edad) {
+        this.name = name;
+        this.edad = edad;
+        
+    }
+}
