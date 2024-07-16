90ae0ed2090ee94e101439bf75feb922e6e34f50
Miguel Ligero <milica0479@gmail.com>
2024-07-16 12:47:16 +0200

    Finish Iterator

--- Java/sheet03/Ex06/Main.java
+++ Java/sheet03/Ex06/Main.java
@@ -4,7 +4,22 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Main {
-    
-}
+    public static void main(String[] args) {
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
 
-// 10, 15, 20 y 25
+        arrList.add(10);
+        arrList.add(15);
+        arrList.add(20);
+        arrList.add(25);
+
+        Iterator<Integer> it = arrList.iterator();
+
+        while (it.hasNext()) {
+            if (it.next() % 2 != 0) {
+                it.remove();
+            }
+        }
+
+        System.out.println("Lista después de eliminar impares: " + arrList);
+    }
+}
