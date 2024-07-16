038d1bc515d32d74515d6fb727fecbc25ea0ebd0
Miguel Ligero <milica0479@gmail.com>
2024-07-16 12:39:36 +0200

    Finish HashSet

--- Java/sheet03/Ex05/Main.java
+++ Java/sheet03/Ex05/Main.java
@@ -1,5 +1,18 @@
 package Java.sheet03.Ex05;
 
+import java.util.HashSet;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        HashSet<Integer> hashset = new HashSet<Integer>();
+
+        hashset.add(10);
+        hashset.add(20);
+        hashset.add(30);
+        hashset.add(10);
+
+        for (Integer number : hashset) {
+            System.out.println(number);
+        }
+    }
 }
