75954c0636ba63c9476b7388b65f5bfc867044af
Miguel Ligero <milica0479@gmail.com>
2024-07-16 12:34:52 +0200

    Finish HashMap

--- Java/sheet03/Ex04/Main.java
+++ Java/sheet03/Ex04/Main.java
@@ -3,6 +3,16 @@ package Java.sheet03.Ex04;
 import java.util.HashMap;
 
 public class Main {
-   
+    public static void main(String[] args) {
+        HashMap<String, Integer> mapList = new HashMap<String, Integer>();
+
+        mapList.put("Ana", 25);
+        mapList.put("Luis", 35);
+        mapList.put("Juan", 30);
+
+        for (String name : mapList.keySet()) {
+            System.out.println(name + " tiene " + mapList.get(name) + " años.");
+        }
+    }
     
 }
