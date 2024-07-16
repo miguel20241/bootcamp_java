8110dda272a097d7ccad0a8978802258dfd72589
Miguel Ligero <milica0479@gmail.com>
2024-07-16 12:16:38 +0200

    Finished LinkedList

--- Java/sheet03/Ex03/Main.java
+++ Java/sheet03/Ex03/Main.java
@@ -3,5 +3,15 @@ package Java.sheet03.Ex03;
 import java.util.LinkedList;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        LinkedList<String> wordsArr = new LinkedList<String>();
+
+        wordsArr.add("Hola");
+        wordsArr.add("Mundo");
+        wordsArr.add("Java");
+
+        for (String word : wordsArr) {
+            System.out.println(word);
+        }
+    }
 }
