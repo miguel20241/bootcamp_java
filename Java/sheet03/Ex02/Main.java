697a958d9b5879cb5a6d73134d9f9616c96077cf
Miguel Ligero <milica0479@gmail.com>
2024-07-16 12:10:23 +0200

    Finished ArrayList

--- Java/sheet03/Ex02/Main.java
+++ Java/sheet03/Ex02/Main.java
@@ -1,5 +1,22 @@
 package Java.sheet03.Ex02;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> numbArr = new ArrayList<Integer>(); 
+
+        numbArr.add(10);
+        numbArr.add(20);
+        numbArr.add(30);
+
+        int total = 0;
+        for (Integer number : numbArr) {
+            total += number;
+        }
+
+        System.out.println("La suma es: " + total);
+
+    }
 }
