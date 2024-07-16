eae50149c49988989e7db7e3b035f7c9531bd4ef
Miguel Ligero <milica0479@gmail.com>
2024-07-16 13:35:56 +0200

    Finish try-catch

--- Java/sheet03/Ex08/Main.java
+++ Java/sheet03/Ex08/Main.java
@@ -1,5 +1,23 @@
 package Java.sheet03.Ex08;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        int number = 10;
+
+        divide(number, 4);
+        divide(number, 0);
+        divide(number, -10);
+    }
+
+    private static void divide(int number, int divisor) {
+        try {
+
+            System.out.println("El resultado de la división de " + number + " entre " + divisor + " es " +  number / divisor);
+            
+        } catch (Exception e) {
+
+            System.out.println("Error: División por 0. " + e.getMessage());
+
+        }
+    }
 }
