3a4cf5bc763fc15850b0384faa8d9cfe4aafc491
Miguel Ligero <milica0479@gmail.com>
2024-07-16 12:55:40 +0200

    Finish Wrapper Classes

--- Java/sheet03/Ex07/Main.java
+++ Java/sheet03/Ex07/Main.java
@@ -1,5 +1,12 @@
 package Java.sheet03.Ex07;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        String cadena = "123";
+        Integer number = Integer.parseInt(cadena);
+
+        number *= 2;
+
+        System.out.println("El resultado es: " + number);
+    }
 }
