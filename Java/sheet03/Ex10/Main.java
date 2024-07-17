4a026eaf7324ca8e2ebca3af8227741254e641c2
Miguel Ligero <milica0479@gmail.com>
2024-07-17 10:05:21 +0200

    Finish Thread

--- Java/sheet03/Ex10/Main.java
+++ Java/sheet03/Ex10/Main.java
@@ -1,5 +1,26 @@
 package Java.sheet03.Ex10;
 
-public class Main {
-    
-}
+public class Main extends Thread {
+    public static void main(String[] args) {
+        Main mainObj = new Main();
+        Thread thread = new Thread(mainObj);
+        
+        thread.start();       
+        
+        System.out.println("Fin del main.");
+    }
+
+    public void run() {
+        try {
+            for (int i = 1; i <= 5; i++) {
+                Thread.sleep(1000);
+                
+                System.out.println(i);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Fin del thread.");
+    }
+}   
